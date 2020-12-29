c21d1e23974d64345f583122f55aef91d6420f04
Keshav Motwani <keshavmot2@gmail.com>
2020-12-29 08:17:41 -0500

    update cache

--- R/cache.R
+++ R/cache.R
@@ -3,20 +3,25 @@
 #' @param file_path file path at which to cache
 #' @param fn function whose result to cache
 #'
+#' @importFrom readr write_csv read_csv
+#' @importFrom qs qsave qread
+#' @importFrom parallel detectCores
+#'
 #' @return cached object
 #' @export
 #'
 #' @examples
 #' NULL
 cache = function(file_path, fn) {
+
   if (grepl(".csv", file_path)) {
     write = readr::write_csv
     read = readr::read_csv
   } else {
     write = function(object, path)
-      qs::qsave(object,  path, nthreads = 32)
+      qs::qsave(object,  path, nthreads = detectCores())
     read = function(path)
-      qs::qread(path, nthreads = 32)
+      qs::qread(path, nthreads = detectCores())
   }
 
   if (!file.exists(file_path)) {
@@ -25,5 +30,7 @@ cache = function(file_path, fn) {
   } else {
     result = read(file_path)
   }
+
   return(result)
+
 }
